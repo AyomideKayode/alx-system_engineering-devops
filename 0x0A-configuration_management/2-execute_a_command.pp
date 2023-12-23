cf08d5af3f5858dbe15c6244216ee0009797d2c5
Ayomide Kayode <ayomidekay7@gmail.com>
2023-12-23 11:37:11 +0100

    Execute a command - Hot fix

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # execute a command to kill process killmenow
 
-exec { 'killmenow':
-  path    => 'usr/bin/pkill',
-  command => 'pkill -f killmenow'
+exec { 'pkill -f killmenow':
+  path  => 'usr/bin/:/usr/local/bin/:/bin/'
 }
