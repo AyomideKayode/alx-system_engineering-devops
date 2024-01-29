76213b26946e9263a84d04101d09f16ecec5ad4d
Ayomide Kayode <ayomidekay7@gmail.com>
2024-01-29 13:23:37 +0100

    Gather Data from API - refactor.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -42,14 +42,14 @@ def get_employee_todo_progress(employee_id):
 
     # Display progress information
     print("Employee {} is done with tasks ({}/{}):".format(
-        employee_name, completed_tasks, total_tasks))
+        employee_name, completed_tasks, total_tasks), end='\n')
     # print("{}: {} completed tasks out of {}".format(
     # employee_name, completed_tasks, total_tasks))
 
     # Display titles of completed tasks
     for task in todos_data:
         if task["completed"]:
-            print("\t{}".format(task['title']))
+            print("\t {}".format(task.get("title")))
 
 
 if __name__ == "__main__":
