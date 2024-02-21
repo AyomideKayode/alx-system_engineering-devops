591bf34d1d6c7d10c23a301f47e24ff4ccb6dfdf
Ayomide <ayomidekay7@gmail.com>
2024-02-21 17:44:24 +0100

    Increase User Limit.

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -9,10 +9,12 @@
 #   2. Restart the Nginx service to apply the new configuration.
 #
 # Note:
-#   - This Puppet script assumes that the 'sudo' command is available for the Puppet user and that it can run commands with elevated privileges.
+#   - This Puppet script assumes that the 'sudo' command is available
+#   for the Puppet user and that it can run commands with elevated privileges.
 #   - Ensure that the 'sudo' configuration allows the Puppet user to execute the necessary commands without requiring a password.
 #   - Verify that modifying the ULIMIT value is appropriate for your system's requirements and resources.
-#   - This script should be run with caution, especially in production environments, as it directly modifies system configuration files and restarts services.
+#   - This script should be run with caution, especially in production environments,
+#    as it directly modifies system configuration files and restarts services.
 
 # Exec resource to replace the ULIMIT value in the Nginx configuration file
 exec {'replace_ulimit':
